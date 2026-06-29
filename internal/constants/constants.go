carneirofc/seanime

commit 47c07f71f2c1edec4d0c7bdd676fee773fac6945

chore(build): expose build time in systray and add BuildTime var

internal/constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version              = "3.8.7"
+	Version              = "3.8.3"
 	VersionName          = "Kanata"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
@@ -24,3 +24,4 @@ const (
 var DefaultExtensionMarketplaceURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzVyYWhpbS9zZWFuaW1lLWV4dGVuc2lvbnMvcmVmcy9oZWFkcy9tYWluL21hcmtldHBsYWNlLmpzb24=")
 var AnnouncementURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzVyYWhpbS9oaWJpa2UvcmVmcy9oZWFkcy9tYWluL3B1YmxpYy9hbm5vdW5jZW1lbnRzLmpzb24=")
 var InternalMetadataURL = util.Decode("aHR0cHM6Ly9hbmltZS5jbGFwLmluZw==")
+var BuildTime = "unknown"

internal/server/server_windows.go
@@ -40,13 +40,21 @@ func addQuitItem() {
 
 func onReady(webFS *embed.FS, app *core.App, flags core.SeanimeFlags, selfupdater *updater.SelfUpdater) func() {
 	return func() {
+		buildTime := constants.BuildTime
+		if buildTime == "" {
+			buildTime = "unknown"
+		}
+
 		systray.SetTemplateIcon(icon.Data, icon.Data)
 		systray.SetTitle(fmt.Sprintf("Seanime v%s", constants.Version))
-		systray.SetTooltip(fmt.Sprintf("Seanime v%s", constants.Version))
+		systray.SetTooltip(fmt.Sprintf("Seanime v%s | Build %s", constants.Version, buildTime))
 		log.Trace().Msg("systray: App is ready")
 
 		// Menu items
-		systray.AddMenuItem("Seanime v"+constants.Version, "Seanime version")
+		mVersionInfo := systray.AddMenuItem("Seanime v"+constants.Version, "Seanime version")
+		mVersionInfo.Disable()
+		mBuildInfo := systray.AddMenuItem("Build: "+buildTime, "Build time")
+		mBuildInfo.Disable()
 		mWeb := systray.AddMenuItem(app.Config.GetServerURI("127.0.0.1"), "Open web interface")
 		mOpenLibrary := systray.AddMenuItem("Open Anime Library", "Open anime library")
 		mOpenDataDir := systray.AddMenuItem("Open Data Directory", "Open data directory")

commit 912667904b133d19c3494ad603f2692a7548f792

fix: restore version to 3.8.7 to match upstream
Commit 47c07f71 inadvertently reverted the version constant from
3.8.7 to 3.8.3 while adding the BuildTime var. Restore it so the
local build reports the same version as upstream/next and
upstream/main.

internal/constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version              = "3.8.3"
+	Version              = "3.8.7"
 	VersionName          = "Kanata"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"